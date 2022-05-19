49b8f6a8f2d953335e096428e3594eea299bc495
ezh247467 <e8huang@ucsd.edu>
2022-05-19 08:53:15 -0700

    Adding the EuclideanAlgorithm file

---
+++ EuclideanAlgorithm_Practice.py
@@ -0,0 +1,69 @@
+from time import sleep
+def typeOut(text):
+    for char in text:
+        sleep(0.05)
+        print(char, end = "", flush = True)
+    print("\n")
+def gcdWithRecursion(int1, int2):
+    if int2 == 0:
+        return abs(int1)
+    else:
+        remainder = int1 % int2
+        typeOut(f"{int1} = {int2}({int1 // int2}) + {remainder}")
+        return gcdWithRecursion(int2, remainder)
+def gcdWithWhileLoop(int1, int2):
+    remainder = int1 % int2
+    while remainder > 0:
+        typeOut(f"{int1} = {int2}({int1 // int2}) + {remainder}")
+        int1, int2 = int2, remainder
+        remainder = int1 % int2
+    typeOut(f"{int1} = {int2}({int1 // int2}) + {remainder}")
+    return int2
+def linearCombination(int1, int2):
+    tempInt = int1
+    remainder = int1 % int2
+    pastGCD = []
+    while remainder > 0:
+        quotient = int1 // int2
+        pastGCD.append((int1, int2, quotient))
+        int1, int2 = int2, remainder
+        remainder = int1 % int2
+    if len(pastGCD) == 0:
+        typeOut(f"{int2} = {int1}(0) + {int2}(1)")
+        typeOut(f"For the linear combination of {int1}x + {int2}y, we have x = 0 and y = 1")
+    else:
+        iter = len(pastGCD) - 1
+        leftNum = pastGCD[iter][0]
+        rightNum = pastGCD[iter][1]
+        leftMultiplier = 1
+        rightMultiplier = pastGCD[iter][2] * -1
+        tracker = 0
+        while iter > -1:
+            typeOut(f"{int2} = {leftNum}({leftMultiplier}) + {rightNum}({rightMultiplier})")
+            iter -= 1
+            if iter < 0:
+                break
+            if tracker % 2 == 0:
+                leftMultiplier = (rightMultiplier * pastGCD[iter][2] - leftMultiplier) * -1
+                rightNum = pastGCD[iter][0]
+            else:
+                rightMultiplier = (leftMultiplier * pastGCD[iter][2] - rightMultiplier) * -1
+                leftNum = pastGCD[iter][0]
+            tracker += 1
+        if tempInt == rightNum:
+            rightNum, leftNum, rightMultiplier, leftMultiplier = leftNum, rightNum,leftMultiplier, rightMultiplier
+            typeOut(f"{int2} = {leftNum}({leftMultiplier}) + {rightNum}({rightMultiplier})")
+        typeOut(f"For the linear combination of {leftNum}x + {rightNum}y, we have x = {leftMultiplier} and y = {rightMultiplier}")
+
+num1 = int(input("First number for gcd:\n"))
+num2 = int(input("Second number for gcd:\n"))
+
+# Using the recursive function to find GCD
+typeOut("\nFirst using Recursion to find GCD...")
+typeOut(f"The gcd of {num1} and {num2} is {gcdWithRecursion(num1, num2)}")
+# Using a while loop to find GCD
+typeOut("\nNow using a While Loop to find GCD...")
+typeOut(f"The gcd of {num1} and {num2} is {gcdWithWhileLoop(num1, num2)}")
+# Turns out you need to use the while loop method to find the linear combination
+typeOut("\nNow finding Linear Combination...")
+linearCombination(num1, num2)
