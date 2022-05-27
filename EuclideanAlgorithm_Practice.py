274292c62b89c0a5e9b5695bc2c1b2e52bc73f65
ezh247467 <e8huang@ucsd.edu>
2022-05-27 12:00:18 -0700

    Created typeOutLine method to differentiate between making a new line and not wanting a new line

--- EuclideanAlgorithm_Practice.py
+++ EuclideanAlgorithm_Practice.py
@@ -3,34 +3,35 @@ def typeOut(text):
     for char in text:
         sleep(0.05)
         print(char, end = "", flush = True)
+def typeOutLine(text):
+    typeOut(text)
     print()
 def gcdWithRecursion(int1, int2):
     if int2 == 0:
         return abs(int1)
     else:
         remainder = int1 % int2
-        typeOut(f"{int1} = {int2}({int1 // int2}) + {remainder}")
+        typeOutLine(f"{int1} = {int2}({int1 // int2}) + {remainder}")
         return gcdWithRecursion(int2, remainder)
 def gcdWithWhileLoop(int1, int2):
     remainder = int1 % int2
     while remainder > 0:
-        typeOut(f"{int1} = {int2}({int1 // int2}) + {remainder}")
+        typeOutLine(f"{int1} = {int2}({int1 // int2}) + {remainder}")
         int1, int2 = int2, remainder
         remainder = int1 % int2
-    typeOut(f"{int1} = {int2}({int1 // int2}) + {remainder}")
+    typeOutLine(f"{int1} = {int2}({int1 // int2}) + {remainder}")
     return int2
 def linearCombination(int1, int2):
     tempInt = int1
     remainder = int1 % int2
     pastGCD = []
     while remainder > 0:
-        quotient = int1 // int2
-        pastGCD.append((int1, int2, quotient))
+        pastGCD.append((int1, int2, int1 // int2))
         int1, int2 = int2, remainder
         remainder = int1 % int2
     if len(pastGCD) == 0:
-        typeOut(f"{int2} = {int1}(0) + {int2}(1)")
-        typeOut(f"For the linear combination of {int1}x + {int2}y, we have x = 0 and y = 1")
+        typeOutLine(f"{int2} = {int1}(0) + {int2}(1)")
+        typeOutLine(f"For the linear combination of {int1}x + {int2}y, we have x = 0 and y = 1")
     else:
         iter = len(pastGCD) - 1
         leftNum = pastGCD[iter][0]
@@ -39,7 +40,7 @@ def linearCombination(int1, int2):
         rightMultiplier = pastGCD[iter][2] * -1
         tracker = 0
         while iter > -1:
-            typeOut(f"{int2} = {leftNum}({leftMultiplier}) + {rightNum}({rightMultiplier})")
+            typeOutLine(f"{int2} = {leftNum}({leftMultiplier}) + {rightNum}({rightMultiplier})")
             iter -= 1
             if iter < 0:
                 break
@@ -52,18 +53,17 @@ def linearCombination(int1, int2):
             tracker += 1
         if tempInt == rightNum:
             rightNum, leftNum, rightMultiplier, leftMultiplier = leftNum, rightNum,leftMultiplier, rightMultiplier
-            typeOut(f"{int2} = {leftNum}({leftMultiplier}) + {rightNum}({rightMultiplier})")
-        typeOut(f"For the linear combination of {leftNum}x + {rightNum}y, we have x = {leftMultiplier} and y = {rightMultiplier}")
-typeOut("First number for gcd:")
+            typeOutLine(f"{int2} = {leftNum}({leftMultiplier}) + {rightNum}({rightMultiplier})")
+        typeOutLine(f"For the linear combination of {leftNum}x + {rightNum}y, we have x = {leftMultiplier} and y = {rightMultiplier}")
+typeOut("First number for gcd: ")
 num1 = int(input())
-typeOut("Second number for gcd:")
+typeOut("Second number for gcd: ")
 num2 = int(input())
 # Using the recursive function to find GCD
-typeOut("\nFirst using Recursion to find GCD...")
-typeOut(f"The gcd of {num1} and {num2} is {gcdWithRecursion(num1, num2)}")
+typeOutLine("\nFirst using Recursion to find GCD...")
+typeOutLine(f"The gcd of {num1} and {num2} is {gcdWithRecursion(num1, num2)}")
 # Using a while loop to find GCD
-typeOut("\nNow using a While Loop to find GCD...")
-typeOut(f"The gcd of {num1} and {num2} is {gcdWithWhileLoop(num1, num2)}")
-# Turns out you need to use the while loop method to find the linear combination
-typeOut("\nNow finding Linear Combination...")
+typeOutLine("\nNow using a While Loop to find GCD...")
+typeOutLine(f"The gcd of {num1} and {num2} is {gcdWithWhileLoop(num1, num2)}")
+typeOutLine("\nNow finding Linear Combination...")
 linearCombination(num1, num2)
